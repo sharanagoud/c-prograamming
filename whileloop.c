539aef12348661b6806b75c9332c1ae7a711378a
sharanagoud <95670375+sharanagoud@users.noreply.github.com>
2021-12-07 10:11:08 +0530

    Add files via upload

---
+++ whileloop.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+int main(){
+ int a;
+ printf("enter your number");
+ scanf("%d", &a);
+ 
+ while(a>0){
+     printf("%d\n", a);
+     a++;
+     if(a==1000){
+         break;
+     }}
+     return 0;
+ }
+
